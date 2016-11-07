445d79056e7fae47cbe818b74054664e8670cace
Aaron Reich <agr505@gmail.com>
2016-11-07 12:35:39 -0500

    Added Account and Seller Classes

--- src/RealEstateFinder/Seller.java
+++ src/RealEstateFinder/Seller.java
@@ -9,14 +9,6 @@ package RealEstateFinder;
  *
  * @author Aaron
  */
-public class Application {
-
-    /**
-     * @param args the command line arguments
-     */
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Seller extends Account{
     
 }
