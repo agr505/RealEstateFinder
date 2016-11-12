390105f918ee2088aabcbd976e178b613e195f4e
Dan1shajk <danishshakil15@yahoo.com>
2016-11-11 20:01:20 -0500

    Load Properties
    loading from serialized text file

--- src/RealEstateFinder/AvailableProperties.java
+++ src/RealEstateFinder/AvailableProperties.java
@@ -5,6 +5,9 @@
  */
 package RealEstateFinder;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -18,14 +21,33 @@ public class AvailableProperties extends PropertyContainer{
     public AvailableProperties(Application app) 
     {
         application=app;
+        
+    }
+    
+    public void loadProperties() throws IOException, ClassNotFoundException{
+        
+          ObjectInputStream in = new ObjectInputStream(
+            new FileInputStream("property.txt"));
+          
+        while (in.readObject()!= null){
+            super.addProperty((Property) in.readObject());
+            
+        }
+        in.close();
+        
     }
     
     public void matchownedProperties()
     {
         Seller seller=(Seller)application.provideLoggedinAccount();
+        
         ArrayList<String> ownedproperties=seller.getOwnedproperties();
+        
        Iterator<Property> iter= getProperties();
+       
        PropertyContainer propertycontainer=new PropertyContainer();
+       
+       
         for(int i=0;i<ownedproperties.size();i++)
         {
           while(iter.hasNext())
