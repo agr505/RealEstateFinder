73aa41aed35ce9a11f3722ea6725434c4f54094f
Aaron Reich <agr505@gmail.com>
2016-11-11 21:36:17 -0500

    CustomerNavigationBar

---
+++ src/RealEstateFinder/CustomerNavigationBar.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RealEstateFinder;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+
+/**
+ *
+ * @author Aaron
+ */
+public class CustomerNavigationBar extends JMenuBar{
+
+   /* public CustomerNavigationBar() {
+        JMenu availablproperties=new  JMenu();
+        this.add(availableproperties);
+         JMenu Favorites=new  JMenu();
+        this.add(Favorites);
+         JMenu logout=new  JMenu();
+        this.add(AvailablePropertiesPageMenu);
+    }*/
+    
+}
