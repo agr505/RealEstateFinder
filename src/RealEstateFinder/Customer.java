7f75561f291968609cd8f984bb953ca9747e432e
Aaron Reich <agr505@gmail.com>
2016-11-18 17:22:47 -0500

    Customer Update propertyinterestedin

--- src/RealEstateFinder/Customer.java
+++ src/RealEstateFinder/Customer.java
@@ -10,9 +10,14 @@ package RealEstateFinder;
  * @author Aaron
  */
 public class Customer extends Account{
-    
+    private String latestpropertyinterestedin;
      Customer(String username, String password) {
         super(username, password);
+        latestpropertyinterestedin=null;
     }
+     public void updatepropertyinterestedin(String latestproperty)
+     {
+         latestpropertyinterestedin=latestproperty;
+     }
     
 }
