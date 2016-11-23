01ae5b878a6ab67ffeb58629b5d4bec2ed05ec72
Dan1shajk <danishshakil15@yahoo.com>
2016-11-22 22:43:13 -0500

    Added sellerPropertyPage

---
+++ src/RealEstateFinder/SellerPropertyPage.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RealEstateFinder;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author User
+ */
+public class SellerPropertyPage extends JFrame{
+    
+    
+    
+    SellerPropertyPage(){
+        
+        
+        
+        //this.add();
+        this.setTitle("Seller Property Page");
+        this.pack();
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
