02cc657645bd1ea4ac46d2154b626ead6742974e
sagiter <ssneider1@gmail.com>
2016-11-28 18:27:23 -0500

    draft of InterestedCustomersPage

--- src/RealEstateFinder/InterestedCustomersPage.java
+++ src/RealEstateFinder/InterestedCustomersPage.java
@@ -6,6 +6,8 @@
 package RealEstateFinder;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.Iterator;
 import javax.swing.*;
@@ -18,13 +20,15 @@ public class InterestedCustomersPage extends JFrame{
     
     private InterestedCustomers interestedcustomers;
     private ArrayList<Customer> interestedCustomersList;
-    
+    JPanel panel = new JPanel();
+    JScrollPane Sb = new JScrollPane(panel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
     InterestedCustomersPage(InterestedCustomers ic,Application app){
         
         SellerNavigationBar bar = new SellerNavigationBar(app);
         interestedcustomers = ic;
         interestedCustomersList = interestedcustomers.getInterestedCustomers();
         Iterator iter=interestedCustomersList.iterator();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
         
         if (interestedcustomers.getInterestedCustomers()!= null){
             System.out.println("getting the interested customers");
@@ -35,21 +39,48 @@ public class InterestedCustomersPage extends JFrame{
             String x =c.getusername();
             String y =c.returnphonenumber();
             String z = c.returnlatestpropertyinterestedin();
-           
-            // PropertyPanel panel = new PropertyPanel();
-           // panel.add(p);
+            SellerPanel p = new SellerPanel(x,y,z);
+            panel.add(p);
         }
         
 
       //  this.add(panel);
         
-        
+        this.add(Sb);
         this.add(bar, BorderLayout.NORTH);
         this.setTitle("Interested Customers Page");
-        //this.pack();
+        this.pack();
         this.setSize(500, 400);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
 }}
+
+class SellerPanel extends JPanel {
+
+
+    SellerPanel(String x, String y, String z) {
+
+        this.setLayout(new BorderLayout());
+
+        
+        
+        
+
+        JLabel latestProp = new JLabel(z);
+        JLabel userName = new JLabel(x);
+        JLabel phoneNum = new JLabel(y);
+        this.add(userName, BorderLayout.NORTH);
+        this.add(phoneNum, BorderLayout.WEST);
+        this.add(latestProp, BorderLayout.CENTER);
+        //validate();
+
+        
+        this.setBackground(Color.white);
+        this.setPreferredSize(new Dimension(250, 150));
+
+        
+
+    }
+}
