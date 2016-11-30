a847199dc23516c0082b71535c61fdcec6c0b8fd
Dan1shajk <danishshakil15@yahoo.com>
2016-11-29 19:27:35 -0500

    SellerPrpertyListingsPage updated

--- src/RealEstateFinder/SellerPropertyListingsPage.java
+++ src/RealEstateFinder/SellerPropertyListingsPage.java
@@ -27,8 +27,7 @@ public class SellerPropertyListingsPage extends JFrame {
 
         SellerNavigationBar bar = new SellerNavigationBar(app);
         //this.setLayout(n);
-        
-     
+
         System.out.println("before In the sellerpage ");
 
         avprop.matchownedProperties(this);
@@ -39,7 +38,6 @@ public class SellerPropertyListingsPage extends JFrame {
         //FlowLayout fl = new FlowLayout(FlowLayout.LEFT, 20, 10);
 
         // this.setLayout(new FlowLayout());
-        
         this.add(bar, BorderLayout.NORTH);
         this.setTitle("Seller Property Page");
         this.pack();
@@ -53,12 +51,12 @@ public class SellerPropertyListingsPage extends JFrame {
         Iterator<Property> iter = pc.getProperties();
 
         System.out.println("In the sellerpage updateview ");
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 
         while (iter.hasNext()) {
-            JPanel panel = new JPanel();
-            panel.setLayout(new BorderLayout());
+            
             Property p = iter.next();
-
             String name = p.getName();
             String pic = p.getPicture();
             String text = p.getText();
@@ -66,23 +64,28 @@ public class SellerPropertyListingsPage extends JFrame {
             JLabel name1 = new JLabel(name);
             panel.add(name1, BorderLayout.NORTH);
 
+            JLabel pic2 = new JLabel();
+            pic2.setIcon(new ImageIcon(pic));
+            panel.add(pic2,BorderLayout.CENTER );
+            
             JLabel name2 = new JLabel(text);
-            panel.add(name2);
-
-            panel.setBackground(Color.red);
-            panel.setPreferredSize(new Dimension(100, 100));
+            panel.add(name2, BorderLayout.SOUTH);
+            
+            
+            
+            //panel.setBackground(Color.red);
+            //panel.setPreferredSize(new Dimension(100, 100));
             validate();
-            this.add(panel);
-            
-            
-        JButton b = new JButton("View Property Description");
-      
+            //this.add(panel);
 
-        this.add(b, BorderLayout.SOUTH);
- 
-PropertyDescriptionPage propertydescriptionpage=new PropertyDescriptionPage(p.getName(), p.getPicture(), p.getText());
-        b.addActionListener(new ViewPropertyButtonListener(p.getName(), propertydescriptionpage,application,avprop));
+            JButton b = new JButton("View Property Description");
+            panel.add(b, BorderLayout.SOUTH);
+            //this.add(b, BorderLayout.SOUTH);
+
+            PropertyDescriptionPage propertydescriptionpage = new PropertyDescriptionPage(p.getName(), p.getPicture(), p.getText());
+            b.addActionListener(new ViewPropertyButtonListener(p.getName(), propertydescriptionpage, application, avprop));
         }
+        this.add(panel);
         System.out.println("While loop ended");
 
     }
